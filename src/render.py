86f5744b2a33163f37df98bff8621ac8259518dc
Willem <1121567132@qq.com>
2022-12-11 09:59:40 +0800

    chore: add some hints and error catch logic

--- src/render.py
+++ src/render.py
@@ -20,109 +20,143 @@ place_holder_path: str = os.path.join(path, 'placeholder.png')
 error_path: str = os.path.join(path, 'error.png')
 place_holder: ImageClass = Image.open(place_holder_path)
 
+
 def render_main() -> None:
-        # https://emojipedia.org/flower-playing-cards/
-        imageTab: DeltaGenerator; dataTab:DeltaGenerator
-        imageTab, dataTab = st.tabs(["🎴 Image", "🗃 Data"])
+    # https://emojipedia.org/flower-playing-cards/
+    imageTab: DeltaGenerator
+    dataTab: DeltaGenerator
+    imageTab, dataTab = st.tabs(["🎴 Image", "🗃 Data"])
 
-        with imageTab:
-            st.header("🎴 Image")
-            originalImgCol: DeltaGenerator; processedImgCol: DeltaGenerator
-            originalImgCol, processedImgCol = st.columns(2)
-            # declared before spinner, so the spinner is under the columns
-            uploaded_file: UploadedFile | None = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'])
+    with imageTab:
+        st.header("🎴 Image")
+        originalImgCol: DeltaGenerator
+        processedImgCol: DeltaGenerator
+        originalImgCol, processedImgCol = st.columns(2)
+        # declared before spinner, so the spinner is under the columns
+        uploaded_file: UploadedFile | None = st.file_uploader(
+            "Upload Image", type=['jpg', 'jpeg', 'png'])
 
-            # response logic can put anywhere unless there are a view need variables
+        # response logic can put anywhere unless there are a view need variables
+        if uploaded_file is not None:
+            filename = uploaded_file.name
+            st.sidebar.info(f'图片导入成功')
+        else:
+            filename: Literal[''] = ""
+
+        with originalImgCol:
             if uploaded_file is not None:
-                filename = uploaded_file.name
-                st.sidebar.info(f'图片导入成功')
+                image: ImageClass = Image.open(uploaded_file)
+                st.image(
+                    image, caption=f'Original Image: {filename}', use_column_width=True)
             else:
-                filename: Literal[''] = ""
+                st.image(place_holder, caption="Original Image",
+                         use_column_width=True)
 
-            with originalImgCol:
-                if uploaded_file is not None:
-                    image: ImageClass = Image.open(uploaded_file)
-                    st.image(image, caption=f'Original Image: {filename}', use_column_width=True)
-                else:
-                    st.image(place_holder, caption="Original Image", use_column_width=True)
+        # the snippet below couldn't be put in the with statement because the spinner will be under the columns
+        if uploaded_file is not None:
+            with st.spinner('请等待图片上传及处理...'):
+                res: Response | None = upload_image_get_response(
+                    uploaded_file, upload_images_url)
+        else:
+            res: Response | None = None
 
-            # the snippet below couldn't be put in the with statement because the spinner will be under the columns
-            if uploaded_file is not None:
-                with st.spinner('请等待图片上传及处理...'):
-                    res: Response | None = upload_image_get_response(uploaded_file, upload_images_url)
-            else:
-                res: Response | None = None
-
-            with processedImgCol:
-                if 'predict_digits' not in st.session_state:
-                    st.session_state['predict_digits'] = None
-                bias = 0
-                previous_predict_digits = 0
-
-                if st.session_state['predict_digits'] is not None:
-                    previous_predict_digits: int = int(st.session_state['predict_digits'][0])
-
-                if res is not None:
-                    if res.status_code == 200 and 'message' not in res.json():
-                        res_urls: list[str] = res.json()['processed_url']
-                        st.session_state['predict_digits'] = res.json()['predict_digits']
-                        if previous_predict_digits is not None:
-                            bias: int = st.session_state['predict_digits'][0] - int(previous_predict_digits)
-                        # face multiple images upload situation, but st.uploader only support upload one image at one time
-                        for res_url in res_urls:
-                            st.image(res_url, caption=f"Processed Image: {filename}", use_column_width=True)
-                    elif res.status_code == 200 and res.json()['message'] == "the engine download pictures failed":
-                        st.sidebar.error(f'引擎下载图片失败，请重新上传')
-                        error: ImageClass = Image.open(error_path)
-                        st.image(error, caption="Processed Image", use_column_width=True)
-                    elif res.status_code == 200 and res.json()['message'] == "the engine predict failed":
-                        st.sidebar.error(f'引擎预测失败，请选择别的图片进行预测')
-                        error: ImageClass = Image.open(error_path)
-                        st.image(error, caption="Processed Image", use_column_width=True)
-                    elif res.status_code == 200 and res.json()['message'] == 'upload to oss failed':
-                        st.sidebar.error(f'图片上传到云端失败，请重新上传')
-                        error: ImageClass = Image.open(error_path)
-                        st.image(error, caption="Processed Image", use_column_width=True)
-                else:
-                    st.image(place_holder, caption="Processed Image", use_column_width=True)
-
-            threshold: int = st.slider('请选择警示阈值：', 0, 100, 10, key='state_threshold')
-            st.markdown(f'当前窗口超过 `{threshold}` 人后，系统进行人数预警')
-            # switch for siren
-            # state logic can put anywhere unless there are a view need variables
-            if 'siren' not in st.session_state:
-                st.session_state['siren'] = True
-            button: bool = st.button('🔊 预警')
-            if button:
-                st.session_state['siren'] = not st.session_state['siren']
-            # siren hint view, need to be put after the button, before other views which need render after the hint
-            if st.session_state['siren'] == True:
-                st.write(f'🔊 预警播报：开')
-            else:
-                st.write(f'🔊 预警播报：关')
+        with processedImgCol:
+            if 'predict_digits' not in st.session_state:
+                st.session_state['predict_digits'] = None
+            bias = 0
+            previous_predict_digits = 0
 
             if st.session_state['predict_digits'] is not None:
-                for predict_digit in st.session_state['predict_digits']:
-                    if predict_digit >= threshold:
-                        st.metric(label="当前预测数值", value=predict_digit, delta=bias)
-                        st.error(f'当前窗口人数为`{predict_digit}`人，已超过 `{threshold}` 人，系统进行人数预警，请注意！', icon="⚠️")
-                        # TODO: HOW TO MAKE DOCKER work
-                        # this link is sharky-backend:8000/static/siren2.mp3, browser can not play it
-                        if st.session_state['siren']:
-                            components.html(
-                                """
+                previous_predict_digits: int = int(
+                    st.session_state['predict_digits'][0])
+
+            if res is not None:
+                if res.status_code == 200 and 'message' not in res.json():
+                    res_urls: list[str] = res.json()['processed_url']
+                    st.session_state['predict_digits'] = res.json()[
+                        'predict_digits']
+                    if previous_predict_digits is not None:
+                        bias: int = st.session_state['predict_digits'][0] - int(
+                            previous_predict_digits)
+                    # face multiple images upload situation, but st.uploader only support upload one image at one time
+                    for res_url in res_urls:
+                        st.image(
+                            res_url, caption=f"Processed Image: {filename}", use_column_width=True)
+
+                elif res.status_code == 200 and res.json()['message'] == "the engine download pictures failed":
+                    st.sidebar.error(f'引擎下载图片失败，请重新上传')
+                    error: ImageClass = Image.open(error_path)
+                    st.image(error, caption="Processed Image",
+                             use_column_width=True)
+
+                elif res.status_code == 200 and res.json()['message'] == "the engine predict failed":
+                    st.sidebar.error(f'引擎预测失败，请选择别的图片进行预测')
+                    error: ImageClass = Image.open(error_path)
+                    st.image(error, caption="Processed Image",
+                             use_column_width=True)
+
+                elif res.status_code == 200 and res.json()['message'] == 'backend upload to oss failed':
+                    st.sidebar.error(f'图片上传到云端失败，请重新上传')
+                    error: ImageClass = Image.open(error_path)
+                    st.image(error, caption="Processed Image",
+                             use_column_width=True)
+
+                elif res.status_code == 200 and res.json()['message'] == 'the engine download pictures failed because the url is wrong':
+                    st.sidebar.error(f'引擎下载图片失败，因后端提供给引擎的链接不存在')
+                    error: ImageClass = Image.open(error_path)
+                    st.image(error, caption="Processed Image",
+                             use_column_width=True)
+
+                elif res.status_code == 200 and res.json()['message'] == 'the engine failed to upload processed file':
+                    st.sidebar.error(f'引擎上传图片失败')
+                    error: ImageClass = Image.open(error_path)
+                    st.image(error, caption="Processed Image",
+                             use_column_width=True)
+            else:
+                st.image(place_holder, caption="Processed Image",
+                         use_column_width=True)
+
+        threshold: int = st.slider(
+            '请选择警示阈值：', 0, 100, 10, key='state_threshold')
+        st.markdown(f'当前窗口超过 `{threshold}` 人后，系统进行人数预警')
+        # switch for siren
+        # state logic can put anywhere unless there are a view need variables
+        if 'siren' not in st.session_state:
+            st.session_state['siren'] = True
+        button: bool = st.button('🔊 预警')
+        if button:
+            st.session_state['siren'] = not st.session_state['siren']
+        # siren hint view, need to be put after the button, before other views which need render after the hint
+        if st.session_state['siren'] == True:
+            st.write(f'🔊 预警播报：开')
+        else:
+            st.write(f'🔊 预警播报：关')
+
+        if st.session_state['predict_digits'] is not None:
+            for predict_digit in st.session_state['predict_digits']:
+                if predict_digit >= threshold:
+                    st.metric(label="当前预测数值", value=predict_digit, delta=bias)
+                    st.error(
+                        f'当前窗口人数为`{predict_digit}`人，已超过 `{threshold}` 人，系统进行人数预警，请注意！', icon="⚠️")
+                    # TODO: HOW TO MAKE DOCKER work
+                    # this link is sharky-backend:8000/static/siren2.mp3, browser can not play it
+                    if st.session_state['siren']:
+                        components.html(
+                            """
                                 <audio autoplay style:"visibility:hidden;position:fixed;">
                                     <source src="%s" type="audio/mpeg">
                                 </audio>
                                 """ % siren_url
-                            )
-                    else:
-                        st.metric(label="当前预测数值", value=predict_digit, delta=bias)
-                        st.success(f'当前窗口人数为`{predict_digit}`人，未超过 `{threshold}` 人，请继续保持', icon="👍")
+                        )
+                else:
+                    st.metric(label="当前预测数值", value=predict_digit, delta=bias)
+                    st.success(
+                        f'当前窗口人数为`{predict_digit}`人，未超过 `{threshold}` 人，请继续保持', icon="👍")
 
-        with dataTab:
-            st.header("🗃 Data")
-            st.markdown("This is the data tab")
+    with dataTab:
+        st.header("🗃 Data")
+        st.markdown("This is the data tab")
+
 
 def render_history() -> None:
     st.header("📜 History")
@@ -133,5 +167,5 @@ def render_history() -> None:
         chart_data = df[['create_time', 'crowd_predict_number']]
         chart_data['create_time'] = pd.to_datetime(chart_data['create_time'])
 
-        st.line_chart(chart_data, x = 'create_time')
+        st.line_chart(chart_data, x='create_time')
         st.table(df)
